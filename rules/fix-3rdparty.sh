edfcdcefc9064b96a7f6a335c336e2f4c9cd31e2
Steve Woods <swoods@vmware.com>
2022-06-09 14:31:01 -0500

    Title: Improve rules
    Why: needed better tagging and tagged contains rules
    
    What: yaml files
    
    How: (list of changes):
    Signed-off-by: Steve Woods <swoods@vmware.com>

--- rules/fix-3rdparty.sh
+++ rules/fix-3rdparty.sh
@@ -1,17 +1,19 @@
 #!/usr/bin/bash
 while read line; do
   fileName="java-$line.yaml"
-  echo "Building $fileName ..../"
-  echo "name: $fileName" > $fileName 
+  name="java-$line"
+  echo "Building $fileName ..."
+  echo "name: $name" > $fileName 
   echo "filetype: (jsp$|java$)" >> $fileName
-  echo "target: line"  echo "name: $line" >> $fileName
-  echo "filetype: (jsp$|java$)" >> $fileName
+  echo "target: line" >> $fileName  
+  echo "name: $line" >> $fileName
   echo "type: regex" >> $fileName
-  echo "defaultpattern: ^.*import(\s*|=)%s.*$" >> $fileName
+  echo "defaultpattern: ^ *import *(io|com|org|net|edu)\.%s" >> $fileName
   echo "advice: Consult 3rd party documentation" >> $fileName
   echo "category: $line" >> $fileName
   echo "tags:" >> $fileName
   echo "- value: $line" >> $fileName
   echo "patterns:" >> $fileName
   echo "- value: $line" >> $fileName
+  echo "#- import com.$line" >> $fileName
 done < third-party.txt
